3980d6e49dd7a9e241593bf16737f413b4aee239
sid9568 <sid20319@gmail.com>
2025-04-26 09:59:14 +0530

    Added logic in class

--- string_cal.rb
+++ string_cal.rb
@@ -1,7 +1,29 @@
 class StringCalculator
    def self.add(numbers)
-     return numbers
+     return 0 if numbers.empty?
+
+    delimiter = /,|\n/
+
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      custom_delimiter = parts[0][2..-1]
+      delimiter = Regexp.escape(custom_delimiter)
+      numbers = parts[1]
+    end
+
+    nums = numbers.split(/#{delimiter}/).map(&:to_i)
+
+    negatives = nums.select { |n| n < 0 }
+    unless negatives.empty?
+      raise "negative numbers not allowed #{negatives.join(', ')}"
+    end
+
+    nums.sum
    end
 end
 
-puts StringCalculator.add("hello")
+puts StringCalculator.add("")
+puts StringCalculator.add("1")
+puts StringCalculator.add("1,5")
+puts StringCalculator.add("1\n2,3")
+puts StringCalculator.add("//;\n1;2")
