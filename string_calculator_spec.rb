ae036c4887ac8e8c4a7a449f672b34f44cd7b927
sid9568 <sid20319@gmail.com>
2025-04-26 11:10:30 +0530

    Rspec updated

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -9,9 +9,32 @@ RSpec.describe StringCalculator do
     it 'returns the number itself when only one number is given' do
       expect(StringCalculator.add("1")).to eq(1)
     end
-
     it 'returns the sum of two numbers separated by comma' do
       expect(StringCalculator.add("1,5")).to eq(6)
+    end
+     it 'returns the sum of two numbers separated by comma' do
+      expect(StringCalculator.add("1,5")).to eq(6)
+    end
+
+    it 'returns the sum of multiple numbers separated by comma' do
+      expect(StringCalculator.add("1,2,3,4")).to eq(10)
+    end
+
+    it 'handles new lines between numbers' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+    it 'supports custom delimiters' do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
+
+    it 'raises an error when negative numbers are present' do
+      expect { StringCalculator.add("1,-2,3,-4") }
+        .to raise_error("negative numbers not allowed -2, -4")
+    end
+
+    it 'supports custom delimiters of multiple characters' do
+      expect(StringCalculator.add("//***\n1***2***3")).to eq(6)
     end
   end
 
