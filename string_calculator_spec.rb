471db5b9f3bca83e61c4a31705deb32bd1c605fa
sid9568 <sid20319@gmail.com>
2025-04-26 10:31:35 +0530

    check 0 for an empty string

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -2,8 +2,16 @@ require_relative "string_cal"
 
 RSpec.describe StringCalculator do
   describe '.add' do
-    it 'helllo' do
-     p "=========hello"
+    it 'returns 0 for an empty string' do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+
+    it 'returns the number itself when only one number is given' do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
+
+    it 'returns the sum of two numbers separated by comma' do
+      expect(StringCalculator.add("1,5")).to eq(6)
     end
   end
 
